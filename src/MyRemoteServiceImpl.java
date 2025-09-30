ecbea98f807c3346f7cbf6b76f2526a2e7b962df
ikrame888 <gouaicheikrame@gmail.com>
2025-09-30 16:44:01 +0100

    add myremote service implementation

--- src/MyRemoteServiceImpl.java
+++ src/MyRemoteServiceImpl.java
@@ -1,2 +1,11 @@
-public class MyRemoteServiceImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+public class MyRemoteServiceImpl extends UnicastRemoteObject implements MyRemoteService {
+    protected MyRemoteServiceImpl() throws RemoteException {
+        super();
+    }
+    @Override
+    public String sayHello() throws RemoteException {
+        return "Hello from the RMI server";
+    }
 }
